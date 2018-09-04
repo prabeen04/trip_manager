793d4702e3f9fa10bbc37f49e81218d48b2e40a9
Prabeen04 <prabeen.strange@gmail.com>
2018-09-04 20:06:39 +0530

    home_page screen created

---
+++ lib/home_page.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+import './src/screens/feed.dart';
+import './src/screens/trips.dart';
+import './src/screens/members.dart';
+import './src/screens/stats.dart';
+import './src/screens/settings.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    FeedScreen(),
+    TripsScreen(),
+    MembersScreen(),
+    StatsScreen(),
+    SettingsScreen()
+  ];
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+                currentAccountPicture: new CircleAvatar(
+                  backgroundImage: new NetworkImage('https://i.ytimg.com/vi/xwa1cBWWCVY/hqdefault.jpg'),
+                ),
+                accountName: Text(
+                  "Prabeen",
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+                ),
+                accountEmail: Text(
+                  "prabeen.strange@gmail.com",
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+                )),
+            ListTile(
+              title: Text('Profile'),
+              trailing: Icon(Icons.person),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (BuildContext context) => ProfileScreen()));
+              },
+            ),
+            ListTile(
+              title: Text('News'),
+              trailing: Icon(Icons.explore),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (BuildContext context) => NewsScreen()));
+              },
+            ),
+            ListTile(
+              title: Text('Form'),
+              trailing: Icon(Icons.edit),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (BuildContext context) => FormScreen()));
+              },
+            ),
+          ],
+        ),
+      ),
+      body: _children[_currentIndex], // new
+      // Bottom Navigator Starts here!!
+
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        onTap: onTabTapped,
+        currentIndex: _currentIndex, // this will be set when a tab is tapped
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.mail),
+            title: Text('Messages'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            title: Text('Chat'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            title: Text('Notification'),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+               title: Text('Settings'
+               ))
+        ],
+      ),
+    );
+  }
+}
