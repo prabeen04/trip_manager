41e23488fcd022d192b577cd1f653fde945be9fe
Prabeen04 <prabeen.strange@gmail.com>
2018-09-04 20:09:39 +0530

    bottom tab icons added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -33,56 +33,56 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-                currentAccountPicture: new CircleAvatar(
-                  backgroundImage: new NetworkImage('https://i.ytimg.com/vi/xwa1cBWWCVY/hqdefault.jpg'),
-                ),
-                accountName: Text(
-                  "Prabeen",
-                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
-                ),
-                accountEmail: Text(
-                  "prabeen.strange@gmail.com",
-                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
-                )),
-            ListTile(
-              title: Text('Profile'),
-              trailing: Icon(Icons.person),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (BuildContext context) => ProfileScreen()));
-              },
-            ),
-            ListTile(
-              title: Text('News'),
-              trailing: Icon(Icons.explore),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (BuildContext context) => NewsScreen()));
-              },
-            ),
-            ListTile(
-              title: Text('Form'),
-              trailing: Icon(Icons.edit),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (BuildContext context) => FormScreen()));
-              },
-            ),
-          ],
-        ),
-      ),
+      // drawer: Drawer(
+      //   child: ListView(
+      //     // Important: Remove any padding from the ListView.
+      //     padding: EdgeInsets.zero,
+      //     children: <Widget>[
+      //       UserAccountsDrawerHeader(
+      //           currentAccountPicture: new CircleAvatar(
+      //             backgroundImage: new NetworkImage('https://i.ytimg.com/vi/xwa1cBWWCVY/hqdefault.jpg'),
+      //           ),
+      //           accountName: Text(
+      //             "Prabeen",
+      //             style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+      //           ),
+      //           accountEmail: Text(
+      //             "prabeen.strange@gmail.com",
+      //             style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+      //           )),
+      //       ListTile(
+      //         title: Text('Profile'),
+      //         trailing: Icon(Icons.person),
+      //         onTap: () {
+      //           // Update the state of the app
+      //           // ...
+      //           // Then close the drawer
+      //           Navigator.pop(context);
+      //           Navigator.of(context).push(MaterialPageRoute(
+      //               builder: (BuildContext context) => ProfileScreen()));
+      //         },
+      //       ),
+      //       ListTile(
+      //         title: Text('News'),
+      //         trailing: Icon(Icons.explore),
+      //         onTap: () {
+      //           Navigator.pop(context);
+      //           Navigator.of(context).push(MaterialPageRoute(
+      //               builder: (BuildContext context) => NewsScreen()));
+      //         },
+      //       ),
+      //       ListTile(
+      //         title: Text('Form'),
+      //         trailing: Icon(Icons.edit),
+      //         onTap: () {
+      //           Navigator.pop(context);
+      //           Navigator.of(context).push(MaterialPageRoute(
+      //               builder: (BuildContext context) => FormScreen()));
+      //         },
+      //       ),
+      //     ],
+      //   ),
+      // ),
       body: _children[_currentIndex], // new
       // Bottom Navigator Starts here!!
 
@@ -92,20 +92,20 @@ class _HomePageState extends State<HomePage> {
         currentIndex: _currentIndex, // this will be set when a tab is tapped
         items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home'),
+            icon: Icon(Icons.rss_feed),
+            title: Text('FEEDS'),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.mail),
-            title: Text('Messages'),
+            icon: Icon(Icons.trip_origin),
+            title: Text('TRIPS'),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            title: Text('Chat'),
+            icon: Icon(Icons.group_work),
+            title: Text('MEMBERS'),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.notifications),
-            title: Text('Notification'),
+            icon: Icon(Icons.table_chart),
+            title: Text('STATS'),
           ),
           BottomNavigationBarItem(
               icon: Icon(Icons.settings),
