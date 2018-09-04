c1a01e94adf9457e46e1001b7f00ff84a76b640b
Prabeen04 <prabeen.strange@gmail.com>
2018-09-04 21:11:51 +0530

    packages and models added

---
+++ lib/src/models/member.dart
@@ -0,0 +1,7 @@
+class Member {
+  String name;
+  DateTime dob;
+  String phone = '';
+  String email = '';
+  String favoriteColor = '';
+}
---
+++ lib/src/models/trip.dart
@@ -0,0 +1,7 @@
+class Trip {
+  String name;
+  DateTime dob;
+  String phone = '';
+  String email = '';
+  String favoriteColor = '';
+}
