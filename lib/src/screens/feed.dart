d41a86b36558b9306a24133270d357fdbc1eab13
Prabeen04 <prabeen.strange@gmail.com>
2018-09-04 20:15:24 +0530

    trips and feed screen created

---
+++ lib/src/screens/feed.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FeedScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(title: Text('Notifications'),),
+      body: Center(
+        child: Text('Notification Screen'),
+      ),
+      )
+    );
+  }
+}
