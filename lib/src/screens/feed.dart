79d249e21846c62dc78e6d331aa614f5f97160b2
Prabeen04 <prabeen.strange@gmail.com>
2018-09-05 22:36:10 +0530

    firebase methods created

--- lib/src/screens/feed.dart
+++ lib/src/screens/feed.dart
@@ -33,18 +33,78 @@ class FeedScreenState extends State<FeedScreen> {
     return user;
   }
 
+  void _signOut() {
+    googleSignIn.signOut();
+    print("User Signed out");
+  }
+
+  void _add() {
+    Map<String, String> data = <String, String>{
+      "name": "Pawan Kumar",
+      "desc": "Flutter Developer"
+    };
+    documentReference.setData(data).whenComplete(() {
+      print("Document Added");
+    }).catchError((e) => print(e));
+  }
+
+  void _delete() {
+    documentReference.delete().whenComplete(() {
+      print("Deleted Successfully");
+      setState(() {});
+    }).catchError((e) => print(e));
+  }
+
+  void _update() {
+    Map<String, String> data = <String, String>{
+      "name": "Pawan Kumar Updated",
+      "desc": "Flutter Developer Updated"
+    };
+    documentReference.updateData(data).whenComplete(() {
+      print("Document Updated");
+    }).catchError((e) => print(e));
+  }
+
+  void _fetch() {
+    documentReference.get().then((datasnapshot) {
+      if (datasnapshot.exists) {
+        setState(() {
+          myText = datasnapshot.data['desc'];
+        });
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    subscription = documentReference.snapshots().listen((datasnapshot) {
+      if (datasnapshot.exists) {
+        setState(() {
+          myText = datasnapshot.data['desc'];
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    subscription?.cancel();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Scaffold(
-        // appBar: AppBar(title: Text('Feed'),),
-      body: Center(
-        child: Text('Feed Screen'),
-      ),
-      floatingActionButton:
-            FancyFab() // This trailing comma makes auto-formatting nicer for build methods.
-        )
-      );
+        child: Scaffold(
+            // appBar: AppBar(title: Text('Feed'),),
+            body: Center(
+              child: Text('Feed Screen'),
+            ),
+            floatingActionButton:
+                FancyFab() // This trailing comma makes auto-formatting nicer for build methods.
+            ));
   }
 }
