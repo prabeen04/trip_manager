46edb2f7ea33bfd6191c4b203029bb3217879645
Prabeen04 <prabeen.strange@gmail.com>
2018-09-05 22:38:16 +0530

    body added

--- lib/src/screens/feed.dart
+++ lib/src/screens/feed.dart
@@ -100,8 +100,70 @@ class FeedScreenState extends State<FeedScreen> {
     return Container(
         child: Scaffold(
             // appBar: AppBar(title: Text('Feed'),),
-            body: Center(
-              child: Text('Feed Screen'),
+            body: new Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  new RaisedButton(
+                    onPressed: () => _signIn()
+                        .then((FirebaseUser user) => print(user))
+                        .catchError((e) => print(e)),
+                    child: new Text("Sign In"),
+                    color: Colors.green,
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.all(10.0),
+                  ),
+                  new RaisedButton(
+                    onPressed: _signOut,
+                    child: new Text("Sign out"),
+                    color: Colors.red,
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.all(10.0),
+                  ),
+                  new RaisedButton(
+                    onPressed: _add,
+                    child: new Text("Add"),
+                    color: Colors.cyan,
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.all(10.0),
+                  ),
+                  new RaisedButton(
+                    onPressed: _update,
+                    child: new Text("Update"),
+                    color: Colors.lightBlue,
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.all(10.0),
+                  ),
+                  new RaisedButton(
+                    onPressed: _delete,
+                    child: new Text("Delete"),
+                    color: Colors.orange,
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.all(10.0),
+                  ),
+                  new RaisedButton(
+                    onPressed: _fetch,
+                    child: new Text("Fetch"),
+                    color: Colors.lime,
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.all(10.0),
+                  ),
+                  myText == null
+                      ? new Container()
+                      : new Text(
+                          myText,
+                          style: new TextStyle(fontSize: 20.0),
+                        )
+                ],
+              ),
             ),
             floatingActionButton:
                 FancyFab() // This trailing comma makes auto-formatting nicer for build methods.
