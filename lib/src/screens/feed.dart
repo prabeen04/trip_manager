cc6e4b70b58b41cfeed3f5f45c1afb7b8fa54044
Prabeen04 <prabeen.strange@gmail.com>
2018-09-05 22:28:12 +0530

    firebase package imported on feed

--- lib/src/screens/feed.dart
+++ lib/src/screens/feed.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import '../widgets/fancy_fab.dart';
 
 class FeedScreen extends StatefulWidget {
