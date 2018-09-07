81c09dda8368ec5580e5679d69aa1ce572e367af
Prabeen04 <prabeen.strange@gmail.com>
2018-09-07 20:57:49 +0530

    phone contact widget added

---
+++ lib/src/utils/phone_contact.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PhoneContact extends StatefulWidget {
+  @override
+  _PhoneContactState createState() => _PhoneContactState();
+}
+
+class _PhoneContactState extends State<PhoneContact> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
