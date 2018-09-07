042656d2b63de94a60d9f9c51f4fe33d07eac775
Prabeen04 <prabeen.strange@gmail.com>
2018-09-07 21:45:07 +0530

    contactDetail widget created

---
+++ lib/src/utils/contact_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ContactDetail extends StatefulWidget {
+  @override
+  _ContactDetailState createState() => _ContactDetailState();
+}
+
+class _ContactDetailState extends State<ContactDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
