eaae4e815bf37a0149389472f4404a3c0cd45ee8
Prabeen04 <prabeen.strange@gmail.com>
2018-09-07 21:13:36 +0530

    contact_services imported

--- lib/src/utils/phone_contact.dart
+++ lib/src/utils/phone_contact.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:contacts_service/contacts_service.dart';
+import 'package:simple_permissions/simple_permissions.dart';
 
 class PhoneContact extends StatefulWidget {
   @override
@@ -6,6 +9,9 @@ class PhoneContact extends StatefulWidget {
 }
 
 class _PhoneContactState extends State<PhoneContact> {
+  Permission permission;
+  Iterable<Contact> _contacts;
+  
   @override
   Widget build(BuildContext context) {
     return Container(
