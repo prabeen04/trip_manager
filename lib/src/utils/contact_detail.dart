f14e844cc117000e1ed2ddb8f1e4eff039418956
Prabeen04 <prabeen.strange@gmail.com>
2018-09-07 21:54:46 +0530

    ContactDetail page implememented

--- lib/src/utils/contact_detail.dart
+++ lib/src/utils/contact_detail.dart
@@ -1,3 +1,4 @@
+import 'package:contacts_service/contacts_service.dart';
 import 'package:flutter/material.dart';
 
 class ContactDetail extends StatelessWidget {
@@ -6,8 +7,41 @@ class ContactDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold(
+        appBar:
+            AppBar(title: Text(_contact.displayName ?? ""), actions: <Widget>[
+          FlatButton(
+              child: Icon(Icons.delete),
+              onPressed: () {
+                // ContactsService.deleteContact(_contact);
+              })
+        ]),
+        body: SafeArea(
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                  title: Text("Name"),
+                  trailing: Text(_contact.givenName ?? "")),
+              ListTile(
+                  title: Text("Middle name"),
+                  trailing: Text(_contact.middleName ?? "")),
+              ListTile(
+                  title: Text("Family name"),
+                  trailing: Text(_contact.familyName ?? "")),
+              ListTile(
+                  title: Text("Prefix"), trailing: Text(_contact.prefix ?? "")),
+              ListTile(
+                  title: Text("Suffix"), trailing: Text(_contact.suffix ?? "")),
+              ListTile(
+                  title: Text("Company"),
+                  trailing: Text(_contact.company ?? "")),
+              ListTile(
+                  title: Text("Job"), trailing: Text(_contact.jobTitle ?? "")),
+              // AddressesTile(_contact.postalAddresses),
+              // ItemsTile("Phones", _contact.phones),
+              // ItemsTile("Emails", _contact.emails)
+            ],
+          ),
+        ));
   }
 }
