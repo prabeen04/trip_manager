6af14665fe31451bf26062bc56fd679c67c6e0ae
Prabeen04 <prabeen.strange@gmail.com>
2018-09-08 13:50:12 +0530

    addresstile widget created

--- lib/src/utils/contact_detail.dart
+++ lib/src/utils/contact_detail.dart
@@ -46,6 +46,44 @@ class ContactDetail extends StatelessWidget {
   }
 }
 
+class AddressesTile extends StatelessWidget {
+  AddressesTile(this._addresses);
+  final Iterable<PostalAddress> _addresses;
+
+  Widget build(BuildContext context) {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          ListTile(title: Text("Addresses")),
+          Column(
+              children: _addresses
+                  .map((a) => Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Column(
+                          children: <Widget>[
+                            ListTile(
+                                title: Text("Street"),
+                                trailing: Text(a.street)),
+                            ListTile(
+                                title: Text("Postcode"),
+                                trailing: Text(a.postcode)),
+                            ListTile(
+                                title: Text("City"), trailing: Text(a.city)),
+                            ListTile(
+                                title: Text("Region"),
+                                trailing: Text(a.region)),
+                            ListTile(
+                                title: Text("Country"),
+                                trailing: Text(a.country)),
+                          ],
+                        ),
+                      ))
+                  .toList())
+        ]);
+  }
+}
+
+
 class ItemsTile extends StatelessWidget {
   ItemsTile(this._title, this._items);
   final Iterable<Item> _items;
