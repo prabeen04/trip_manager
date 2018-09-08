470207e05bb9791c858d69ecf21e00184f931cbb
Prabeen04 <prabeen.strange@gmail.com>
2018-09-08 14:00:56 +0530

    create_cotact body added

--- lib/src/utils/create_contact.dart
+++ lib/src/utils/create_contact.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:contacts_service/contacts_service.dart';
 
 class CreateContact extends StatefulWidget {
   @override
@@ -6,10 +7,81 @@ class CreateContact extends StatefulWidget {
 }
 
 class _CreateContactState extends State<CreateContact> {
+  Contact contact = Contact();
+  PostalAddress address = PostalAddress(label: "Home");
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add a contact"),
+        actions: <Widget>[
+          FlatButton(
+              onPressed: () {
+                _formKey.currentState.save();
+                contact.postalAddresses = [address];
+                ContactsService.addContact(contact);
+                Navigator.of(context).pop();
+              },
+              child: Icon(Icons.save, color: Colors.white))
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.all(12.0),
+        child: Form(
+            key: _formKey,
+            child: ListView(
+              children: <Widget>[
+                TextFormField(
+                    decoration: const InputDecoration(labelText: 'First name'),
+                    onSaved: (v) => contact.givenName = v),
+                TextFormField(
+                    decoration: const InputDecoration(labelText: 'Middle name'),
+                    onSaved: (v) => contact.middleName = v),
+                TextFormField(
+                    decoration: const InputDecoration(labelText: 'Last name'),
+                    onSaved: (v) => contact.familyName = v),
+                TextFormField(
+                    decoration: const InputDecoration(labelText: 'Prefix'),
+                    onSaved: (v) => contact.prefix = v),
+                TextFormField(
+                    decoration: const InputDecoration(labelText: 'Suffix'),
+                    onSaved: (v) => contact.suffix = v),
+                TextFormField(
+                    decoration: const InputDecoration(labelText: 'Phone'),
+                    onSaved: (v) =>
+                        contact.phones = [Item(label: "mobile", value: v)],
+                    keyboardType: TextInputType.phone),
+                TextFormField(
+                    decoration: const InputDecoration(labelText: 'E-mail'),
+                    onSaved: (v) =>
+                        contact.emails = [Item(label: "work", value: v)],
+                    keyboardType: TextInputType.emailAddress),
+                TextFormField(
+                    decoration: const InputDecoration(labelText: 'Company'),
+                    onSaved: (v) => contact.company = v),
+                TextFormField(
+                    decoration: const InputDecoration(labelText: 'Job'),
+                    onSaved: (v) => contact.jobTitle = v),
+                TextFormField(
+                    decoration: const InputDecoration(labelText: 'Street'),
+                    onSaved: (v) => address.street = v),
+                TextFormField(
+                    decoration: const InputDecoration(labelText: 'City'),
+                    onSaved: (v) => address.city = v),
+                TextFormField(
+                    decoration: const InputDecoration(labelText: 'Region'),
+                    onSaved: (v) => address.region = v),
+                TextFormField(
+                    decoration: const InputDecoration(labelText: 'Postal code'),
+                    onSaved: (v) => address.postcode = v),
+                TextFormField(
+                    decoration: const InputDecoration(labelText: 'Country'),
+                    onSaved: (v) => address.country = v),
+              ],
+            )),
+      ),
     );
   }
 }
