142dec834196cf664949a5549fcfa8a1be89eb60
Prabeen04 <prabeen.strange@gmail.com>
2018-09-08 13:57:47 +0530

    create_contact widget created

---
+++ lib/src/utils/create_contact.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateContact extends StatefulWidget {
+  @override
+  _CreateContactState createState() => _CreateContactState();
+}
+
+class _CreateContactState extends State<CreateContact> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
