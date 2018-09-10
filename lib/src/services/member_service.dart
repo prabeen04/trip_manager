135553a56f4da71ae404d2f4b7afcfb317ee7783
Prabeen04 <prabeen.strange@gmail.com>
2018-09-10 22:29:23 +0530

    -toJson method created

--- lib/src/services/member_service.dart
+++ lib/src/services/member_service.dart
@@ -1,9 +1,21 @@
 import 'dart:async';
-
+import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
+import '../models/member.dart';
 
 class MemberService{
   Future createMember(member) async{
-    return await Firestore.instance.collection('members').add(member);    
+    return await Firestore.instance.collection('members/member').add(member);    
+  }
+  String _toJson(Member contact) {
+    var mapData = new Map();
+    mapData["name"] = contact.name;
+    mapData["dob"] = new DateFormat.yMd().format(contact.dob);
+    mapData["phone"] = contact.phone;
+    mapData["email"] = contact.email;
+    mapData["favoriteColor"] = contact.favoriteColor;
+    String json = json.encode(mapData);
+    return json;
   }
 }
