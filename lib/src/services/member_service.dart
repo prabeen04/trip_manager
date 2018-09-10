e70948504b544ab9ab26a94a506b04ef82ee9c4d
Prabeen04 <prabeen.strange@gmail.com>
2018-09-10 22:31:09 +0530

    bug fixes

--- lib/src/services/member_service.dart
+++ lib/src/services/member_service.dart
@@ -1,21 +1,20 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:intl/intl.dart';
 import '../models/member.dart';
 
 class MemberService{
   Future createMember(member) async{
-    return await Firestore.instance.collection('members/member').add(member);    
+          String val = _toJson(member);
+
+    return await Firestore.instance.collection('members').add(val);    
   }
-  String _toJson(Member contact) {
+  String _toJson(Member member) {
     var mapData = new Map();
-    mapData["name"] = contact.name;
-    mapData["dob"] = new DateFormat.yMd().format(contact.dob);
-    mapData["phone"] = contact.phone;
-    mapData["email"] = contact.email;
-    mapData["favoriteColor"] = contact.favoriteColor;
-    String json = json.encode(mapData);
-    return json;
+    mapData["name"] = member.name;
+    mapData["phone"] = member.phone;
+    mapData["email"] = member.email;
+    String value = json.encode(mapData);
+    return value;
   }
 }
