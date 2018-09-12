a4a79e462a2c4cc083e4cca27a6ff4b17d27e642
Prabeen04 <prabeen.strange@gmail.com>
2018-09-12 19:54:01 +0530

    appbar added

--- lib/src/screens/user/user_screen.dart
+++ lib/src/screens/user/user_screen.dart
@@ -4,6 +4,9 @@ class UserScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('User Profile'),
+      ),
       body: Center(
         child: Text('User screen')
       ) ,
