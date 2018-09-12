080da26c3f959028c16b76f27418764158ddc865
Prabeen04 <prabeen.strange@gmail.com>
2018-09-12 19:50:33 +0530

    setting screen changed to user screen

--- lib/src/screens/user/user.dart
+++ lib/src/screens/user/user.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-class Settings extends StatelessWidget {
+class User extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('setting screen')
+        child: Text('User screen')
       ) ,
       
     );
