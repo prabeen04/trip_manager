0d31c7d2f39ab98d13fabac7bade87b9a33ef0cb
Prabeen04 <prabeen.strange@gmail.com>
2018-09-13 18:53:09 +0530

    swipe list stateful class created

---
+++ lib/src/screens/stats/swipe_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SwipeList extends StatefulWidget {
+  @override
+  _SwipeListState createState() => _SwipeListState();
+}
+
+class _SwipeListState extends State<SwipeList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
