e6b57ef14a82a34ae2a68fd23c77fd0cb1cd4418
Prabeen04 <prabeen.strange@gmail.com>
2018-09-13 18:48:50 +0530

    bug fixes

--- lib/src/services/trip_service.dart
+++ lib/src/services/trip_service.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/trip.dart';
 
 class TripService {
   Future addTrip(trip) async {
-    return await Firestore.instance.collection('trips').add(trip);
+    // return await Firestore.instance.collection('trips').add(trip);
   }
 }
