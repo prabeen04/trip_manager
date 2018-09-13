bb02f9bdd1f2dd904590a456b98b652b92ef8a8c
Prabeen04 <prabeen.strange@gmail.com>
2018-09-13 19:26:00 +0530

    swipelist commented

--- lib/src/screens/stats/swipe_list.dart
+++ lib/src/screens/stats/swipe_list.dart
@@ -1,22 +1,22 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class SwipeList extends StatefulWidget {
-  @override
-  _SwipeListState createState() => _SwipeListState();
-}
+// class SwipeList extends StatefulWidget {
+//   @override
+//   _SwipeListState createState() => _SwipeListState();
+// }
 
-class _SwipeListState extends State<SwipeList> {
-  List<String> list = new List<String>.generate(30, (i) => 'this is a list');
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: list.length,
-        itemBuilder: (BuildContext context, int index){
-          return Dismissible(
-            key: Key(list[index]),
-            child: ListTile(title: Text(list[index])),
-          );
-        },
-      );
-  }
-}
+// class _SwipeListState extends State<SwipeList> {
+//   List<String> list = new List<String>.generate(30, (i) => 'this is a list');
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListView.builder(
+//         itemCount: list.length,
+//         itemBuilder: (BuildContext context, int index){
+//           return Dismissible(
+//             key: Key(list[index]),
+//             child: ListTile(title: Text(list[index])),
+//           );
+//         },
+//       );
+//   }
+// }
