1082287660df261a81cc1f0f2e8606cbf42636c4
Prabeen04 <prabeen.strange@gmail.com>
2018-09-18 20:57:20 +0530

    date and time formated

--- lib/src/services/trip_service.dart
+++ lib/src/services/trip_service.dart
@@ -1,15 +1,17 @@
 import 'dart:async';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 import 'package:firebase_database/firebase_database.dart';
 import '../models/trip.dart';
 
 class TripService {
   Future addTrip(trip) async {
-    return FirebaseDatabase.instance
-        .reference()
-        .push()
-        .child('trips')
-        .set(_toJson(trip));
+    print(_toJson(trip));
+    // return FirebaseDatabase.instance
+    //     .reference()
+    //     .push()
+    //     .child('trips')
+    //     .set(_toJson(trip));
   }
 
   Trip _fromJson(String jsonData) {
@@ -26,9 +28,11 @@ class TripService {
 
   String _toJson(Trip trip) {
     var mapData = new Map();
+
+      // DateTime now = new DateTime.now();
     mapData["name"] = trip.name;
-    mapData["startDate"] = trip.startDate;
-    mapData["endDate"] = trip.endDate;
+    mapData["startDate"] = new DateFormat.yMd().format(trip.startDate);
+    mapData["endDate"] = new DateFormat.yMd().format(trip.endDate);
     mapData["location"] = trip.location;
     mapData["budget"] = trip.budget;
     mapData["members"] = trip.members;
